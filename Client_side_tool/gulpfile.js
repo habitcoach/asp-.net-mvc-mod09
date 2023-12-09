9698a0757fb93af0eb741a93e9507ea65eae8da7
habitcoach <the_habit_coach@outlook.com>
2023-12-09 20:11:07 +0530

    Add project files.

---
+++ Client_side_tool/gulpfile.js
@@ -0,0 +1,36 @@
+﻿const gulp = require('gulp');
+//const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+
+gulp.task('sass', function () {
+    console.log('wow, it is working!!!!!!!!!!');
+    return gulp.src('wwwroot/sass/sassSite.scss') // Path to your Sass files
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css/')); // Destination folder for the compiled CSS
+});
+
+;
+
+// Task to minify CSS
+gulp.task('minifyCss', function () {
+    return gulp.src('wwwroot/css/site.css') // Replace with the path to your CSS file
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('wwwroot/css')); // Output folder for the minified CSS
+});
+
+// Task to concatenate and minify CSS files
+gulp.task('bundleCss', function () {
+    return gulp.src(['wwwroot/sass/sassSite.scss', 'wwwroot/css/site.css']) // List your CSS files here
+        .pipe(concat('bundle.css')) // Concatenate into a single file
+        .pipe(cleanCSS()) // Minify the concatenated CSS
+        .pipe(gulp.dest('wwwroot/css')); // Output folder for the bundled and minified CSS
+});
+
+gulp.task('test', function () {
+
+    console.log('wow, it is working!');
+
+});
+
---
+++ mod09/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mod09.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
